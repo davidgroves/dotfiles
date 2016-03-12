96c40a4d01875d2b896a4d94468a7255697b44ed
David Groves <github@fibrecat.org>
2016-03-12 19:58:33 +0000

    Initial commit

---
+++ install-dotfiles.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DIR=~/dotfiles
+BACKDIR=~/dotfiles_old
+
+# Create backups of old dotfiles
+mkdir -p $BACKDIR
+
+# Install
+cd $DIR
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for FILE in .*; do
+    echo "Backing up $FILE"
+    mv ~/.$FILE $BACKDIR
+    echo "Creating symlink to $FILE in $HOME"
+    ln -s $dir/$file ~/.$file
+done
